70eda0c4f5fac52faad98c6120035797b88e8e4f
NolanRangel <90576911+NolanRangel@users.noreply.github.com>
2022-02-27 11:56:20 -0700

    committing for branch pull

--- client/src/components/deleteForm/DeleteForm.jsx
+++ client/src/components/deleteForm/DeleteForm.jsx
@@ -19,7 +19,7 @@ const DeleteForm = () => {
   return (
 
     <div className={`d-flex flex-column align-items-center p-4 ${styles.paper}`} >
-        <h2 className='mt-2'>Are you sure you want to delete your Ad:</h2>
+        <h2 className='mt-2'>Are you sure you want to delete your Ad?</h2>
         <h4 className={styles.title}>Advertisement Title</h4>
             <div className={`d-flex justify-content-center mt-3 p-3 ${styles.container}`} >
                 <form>
