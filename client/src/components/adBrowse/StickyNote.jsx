d52e15e18011888cbf2db72aa2370837be0c8b34
Thomas Arndt <arndtt42@gmail.com>
2022-03-01 13:37:36 -0800

    Display of images

--- client/src/components/adBrowse/StickyNote.jsx
+++ client/src/components/adBrowse/StickyNote.jsx
@@ -6,7 +6,7 @@ const StickyNote = ({ ad }) => {
     const image = require(`../../static/images/adImages/${ad.image}`)
 
     return (
-        <a href="/details/0" className={styles.stickyNote} >
+        <a href={`/details/${ad.id}`} className={styles.stickyNote} >
             <img src={image} alt="Advertisement Image" className={styles.image} />
             <h6 className={styles.title}>{ad.title}</h6>
             <p>${ad.price}</p>
